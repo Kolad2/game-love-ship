a965792934f750dcb72220d0b4960e1d0a0670ea
Koladik <koladik@gmail.com>
2026-09-19 02:14:13 +0300

    fix require relative

--- src/game/init.lua
+++ src/game/init.lua
@@ -1,9 +1,6 @@
-local require_relative = require("src.tools.require_relative")
+local require = require("src.tools.require_relative")
 local Game = {}
-----@diagnostic disable-next-line: duplicate-set-field
-package.loaded[require_relative.path(".")] = Game
-----@diagnostic disable-next-line: duplicate-set-field
-package.loaded[require_relative.path(".init")] = Game
---package.loaded["src.game.init"] = Game
-require_relative(".game")
+package.loaded[require.path(".")] = Game
+package.loaded[require.path(".init")] = Game
+require(".game")
 return Game
