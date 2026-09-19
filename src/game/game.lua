0996198a6a9d602e2b41e2ab4cf10448df764fa7
Koladik <koladik@gmail.com>
2026-09-19 23:56:56 +0300

    restruct

--- src/game/game.lua
+++ src/game/game.lua
@@ -1,19 +1,10 @@
 local require = require("src.tools.require_relative")
 local Game = require(".init")
-
-local TextureAtlas = require("src.engine.texture_atlas")
-local Sprite = require("src.engine.sprite")
-local ShipEngine = require(".engines.ship_engine")
-local DirectionalEngine = require(".engines.directional_engine")
-local Texture = require("src.engine.texture")
-local GameObject = require(".object")
-local AnimationTrack = require("src.engine.animation.animation_track")
-local Animation = require("src.engine.animation.animation")
-local AnimationPlayer = require("src.engine.animation.animation_player")
+local init_objects = require(".init_game")
 
 ---@class Game
 ---@field ui UI
-Game = Game or {}
+local Game = Game or {}
 Game.__index = Game
 
 function Game:update(dt)
@@ -37,7 +28,6 @@ end
 
 function Game:init(ui)
     self.ui = ui
-    self.speed = 120
     self.ui.button.pressed:subscribe(function()
     	self:_on_button_pressed()
     end)
@@ -48,62 +38,10 @@ function Game:init(ui)
 end
 
 function Game:init_objects()
-	local image = love.graphics.newImage("assets/spaceship_ant.png")
-    local texture = Texture:create(image)
-    texture.angle = math.pi / 2
-    local sprite = Sprite:create(texture)
-    sprite.sx = 0.5
-    sprite.sy = 0.5
-    local ship_engine = ShipEngine:create(ui.joystick)
-    local ship = GameObject:create(250, 250, sprite, ship_engine)
-    
-    local sprite_sheet = TextureAtlas:create("assets/plasma_bullet.png", 80, 64)
-    local quad_texture = sprite_sheet:get_texture(2)
-    local sprite = Sprite:create(quad_texture)
-    local engine = DirectionalEngine:create(10, 0)
-
-    local animation = {}
-    do
-        animation["birth"] = Animation:create(0.25, true)
-        local track = AnimationTrack:create({"sprite", "texture"})
-        track:insert_key(sprite_sheet:get_texture(1), 0)
-        track:insert_key(sprite_sheet:get_texture(2), 0.05)
-        track:insert_key(sprite_sheet:get_texture(3), 0.10)
-        track:insert_key(sprite_sheet:get_texture(4), 0.15)
-        track:insert_key(sprite_sheet:get_texture(5), 0.20)
-        animation["birth"]:add_track(track)
+	local objects = init_objects()
+    for i, object in ipairs(objects) do
+        table.insert(self.objects, object)
     end
-
-    do
-        animation["fly"] = Animation:create(0.25, true)
-        local track = AnimationTrack:create({"sprite", "texture"})
-        track:insert_key(sprite_sheet:get_texture(6), 0)
-        track:insert_key(sprite_sheet:get_texture(7), 0.05)
-        track:insert_key(sprite_sheet:get_texture(8), 0.10)
-        track:insert_key(sprite_sheet:get_texture(9), 0.15)
-        track:insert_key(sprite_sheet:get_texture(10), 0.20)
-        animation["fly"]:add_track(track)
-    end
-
-    do
-        animation["death"] = Animation:create(0.25, true)
-        local track = AnimationTrack:create({"sprite", "texture"})
-        track:insert_key(sprite_sheet:get_texture(11), 0)
-        track:insert_key(sprite_sheet:get_texture(12), 0.05)
-        track:insert_key(sprite_sheet:get_texture(13), 0.10)
-        track:insert_key(sprite_sheet:get_texture(14), 0.15)
-        track:insert_key(sprite_sheet:get_texture(15), 0.20)
-        animation["death"]:add_track(track)
-    end
-
-    local animation_player = AnimationPlayer:create()
-    local bullet = GameObject:create(250, 250, sprite, engine)
-
-    animation_player:play(bullet, animation["death"])
-    
-    table.insert(self.objects, ship)
-    table.insert(self.objects, bullet)
-    table.insert(self.objects, animation_player)
 end
 
 function Game.create(cls, ...)
---
+++ src/game/init_game.lua
@@ -0,0 +1,80 @@
+local require = require("src.tools.require_relative")
+local TextureAtlas = require("src.engine.texture_atlas")
+local Sprite = require("src.engine.sprite")
+local ShipEngine = require(".engines.ship_engine")
+local DirectionalEngine = require(".engines.directional_engine")
+local Texture = require("src.engine.texture")
+local GameObject = require(".object")
+local AnimationTrack = require("src.engine.animation.animation_track")
+local Animation = require("src.engine.animation.animation")
+local AnimationPlayer = require("src.engine.animation.animation_player")
+
+
+local function init_bullet_animations(sprite_sheet)
+    local animation = {}
+    do
+        animation["birth"] = Animation:create(0.25, true)
+        local track = AnimationTrack:create({"sprite", "texture"})
+        track:insert_key(sprite_sheet:get_texture(1), 0)
+        track:insert_key(sprite_sheet:get_texture(2), 0.05)
+        track:insert_key(sprite_sheet:get_texture(3), 0.10)
+        track:insert_key(sprite_sheet:get_texture(4), 0.15)
+        track:insert_key(sprite_sheet:get_texture(5), 0.20)
+        animation["birth"]:add_track(track)
+    end
+
+    do
+        animation["fly"] = Animation:create(0.25, true)
+        local track = AnimationTrack:create({"sprite", "texture"})
+        track:insert_key(sprite_sheet:get_texture(6), 0)
+        track:insert_key(sprite_sheet:get_texture(7), 0.05)
+        track:insert_key(sprite_sheet:get_texture(8), 0.10)
+        track:insert_key(sprite_sheet:get_texture(9), 0.15)
+        track:insert_key(sprite_sheet:get_texture(10), 0.20)
+        animation["fly"]:add_track(track)
+    end
+
+    do
+        animation["death"] = Animation:create(0.25, true)
+        local track = AnimationTrack:create({"sprite", "texture"})
+        track:insert_key(sprite_sheet:get_texture(11), 0)
+        track:insert_key(sprite_sheet:get_texture(12), 0.05)
+        track:insert_key(sprite_sheet:get_texture(13), 0.10)
+        track:insert_key(sprite_sheet:get_texture(14), 0.15)
+        track:insert_key(sprite_sheet:get_texture(15), 0.20)
+        animation["death"]:add_track(track)
+    end
+    return animation
+end
+
+local function init()
+    local objects = {}
+	local image = love.graphics.newImage("assets/spaceship_ant.png")
+    local texture = Texture:create(image)
+    texture.angle = math.pi / 2
+    local sprite = Sprite:create(texture)
+    sprite.sx = 0.5
+    sprite.sy = 0.5
+    local ship_engine = ShipEngine:create(ui.joystick)
+    local ship = GameObject:create(250, 250, sprite, ship_engine)
+
+    local sprite_sheet = TextureAtlas:create("assets/plasma_bullet.png", 80, 64)
+    local quad_texture = sprite_sheet:get_texture(2)
+    local sprite = Sprite:create(quad_texture)
+    local engine = DirectionalEngine:create(10, 0)
+
+    local animation = init_bullet_animations(sprite_sheet)
+
+    local animation_player = AnimationPlayer:create()
+    local bullet = GameObject:create(250, 250, sprite, engine)
+
+    animation_player:play(bullet, animation["death"])
+
+    table.insert(objects, ship)
+    table.insert(objects, bullet)
+    table.insert(objects, animation_player)
+    return objects
+end
+
+
+return init
